02a7560b5dc0f30e4e961e559592975f4230ec94
Miquel Angel <hulkrojo2@gmail.com>
2022-06-06 14:38:36 +0200

    added main methods and message when plugin is loading

--- src/main/java/es/hulk/survival/Survival.java
+++ src/main/java/es/hulk/survival/Survival.java
@@ -1,11 +1,19 @@
 package es.hulk.survival;
 
+import dev.panda.chat.ChatUtil;
+import dev.panda.file.FileConfig;
+import dev.panda.rank.RankManager;
+import es.hulk.survival.commands.SurvivalCommand;
+import es.hulk.survival.hooks.ScoreboardHook;
+import es.hulk.survival.hooks.TablistHook;
+import es.hulk.survival.utils.command.BaseCommand;
 import es.hulk.survival.utils.command.CommandManager;
-import es.hulk.survival.utils.scoreboard.Scoreboard;
-import es.hulk.tablist.Omega;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Getter @Setter
 public final class Survival extends JavaPlugin {
@@ -13,17 +21,49 @@ public final class Survival extends JavaPlugin {
     @Getter private static Survival instance;
 
     private CommandManager commandManager;
-    private Scoreboard scoreboard;
-    private Omega tablist;
+    private RankManager rankManager;
+    private FileConfig mainConfig;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        this.mainConfig = new FileConfig(this, "settings.yml");
+        this.loadManagers();
+
+        rankManager.loadRank();
+
+        ChatUtil.log("");
+        ChatUtil.log("&aSurvival &e- &f" + getDescription().getVersion());
+        ChatUtil.log("");
+        ChatUtil.log("&aAuthor&7: &f" + getDescription().getAuthors());
+        ChatUtil.log("&aVersion&7: &f1.18.1");
+        ChatUtil.log("&aRank System&7: &f" + rankManager.getRankSystem());
+        ChatUtil.log("");
+        ScoreboardHook.init(this);
+        TablistHook.init(this);
+        loadCommands();
+        ChatUtil.log("");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        ScoreboardHook.getScoreboard().getBoards().clear();
+        TablistHook.getTablist().disable();
+    }
+
+    private void loadManagers() {
+        this.commandManager = new CommandManager(this);
+        this.rankManager = new RankManager(this);
+    }
+
+    private void loadCommands() {
+        List<BaseCommand> commandList = Arrays.asList(new SurvivalCommand());
+
+        for (BaseCommand command : commandList) {
+            CommandManager.getInstance().registerCommands(command, null);
+        }
+
+        ChatUtil.log("&aLoaded &f" + commandList.size() + " &acommands!");
     }
 }
