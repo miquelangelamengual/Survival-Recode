d722731c705dde347d61bcf90005ca1b7a305031
Miquel Angel <hulkrojo2@gmail.com>
2022-06-06 14:39:01 +0200

    started making tablist

--- src/main/java/es/hulk/survival/providers/TablistProvider.java
+++ src/main/java/es/hulk/survival/providers/TablistProvider.java
@@ -1,25 +1,36 @@
 package es.hulk.survival.providers;
 
+import com.google.common.collect.Sets;
+import es.hulk.survival.utils.PlayerUtils;
 import es.hulk.tablist.TablistAdapter;
 import es.hulk.tablist.TablistLayout;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class TablistProvider implements TablistAdapter {
     @Override
     public Set<TablistLayout> getProvider(Player player) {
-        return null;
+        Set<TablistLayout> layoutSet = Sets.newHashSet();
+        PlayerUtils playerUtils = new PlayerUtils(player);
+        return layoutSet;
     }
 
     @Override
     public List<String> getFooter(Player player) {
-        return null;
+        return Arrays.asList(
+                "",
+                "&eBienvendo &a" + player.getName() + " &eal Survival",
+                "");
     }
 
     @Override
     public List<String> getHeader(Player player) {
-        return null;
+        return Arrays.asList(
+                "",
+                "&eDisfruta de tu estancia en el servidor",
+                "");
     }
 }
