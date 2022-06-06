b8952c5fcab6f8aa7175fe58ca6cf74e174a4d2a
Miquel Angel <hulkrojo2@gmail.com>
2022-06-06 14:37:08 +0200

    PlayerUtil class to get some stats

---
+++ src/main/java/es/hulk/survival/utils/PlayerUtils.java
@@ -0,0 +1,31 @@
+package es.hulk.survival.utils;
+
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+
+public class PlayerUtils {
+
+    private final Player player;
+
+    public PlayerUtils(Player player) {
+        this.player = player;
+    }
+
+    public int getDeaths() {
+        return player.getStatistic(Statistic.DEATHS);
+    }
+
+    public int getKills() {
+        return player.getStatistic(Statistic.DEATHS);
+    }
+
+    public String getPlayTime() {
+        long ticks = player.getStatistic(Statistic.PLAY_ONE_MINUTE);
+
+        long hour = ((ticks / 20) / 60) / 60;
+        long minute = ticks / 1200 - hour * 60;
+        long seconds = ticks / 20 - (hour * 60 * 60) - (minute * 60);
+
+        return hour + ":" + minute + ":" + seconds;
+    }
+}
