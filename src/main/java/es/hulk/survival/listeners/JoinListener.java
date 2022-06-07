25f252e7de178e8dac1a617bdcbfe4528b0a547f
Miquel Angel <hulkrojo2@gmail.com>
2022-06-07 18:24:08 +0200

    changed position of quit listener

--- src/main/java/es/hulk/survival/listeners/JoinListener.java
+++ src/main/java/es/hulk/survival/listeners/JoinListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class JoinListener implements Listener {
@@ -17,14 +18,14 @@ public class JoinListener implements Listener {
         event.setJoinMessage(ChatUtil.translate("&e" + player.getDisplayName() + " &aa entrado al servidor para jugar un poquito."));
 
         if (!player.hasPlayedBefore()) {
-            player.getInventory().addItem(new ItemStack(Material.STONE_SWORD));
-            player.getInventory().addItem(new ItemStack(Material.STONE_PICKAXE));
-            player.getInventory().addItem(new ItemStack(Material.STONE_AXE));
-            player.getInventory().addItem(new ItemStack(Material.STONE_SHOVEL));
             player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF, 32));
-
             Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "recipe give " + player.getName() + " *");
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(ChatUtil.translate("&e" + event.getPlayer().getDisplayName() + " &aa salido del servidor."));
     }
 
 }
