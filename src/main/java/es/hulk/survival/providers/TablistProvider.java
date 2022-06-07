7bff0e4020f6da6b09f85c9401997d7184fef120
Miquel Angel <hulkrojo2@gmail.com>
2022-06-07 18:24:26 +0200

    updated tablist colors

--- src/main/java/es/hulk/survival/providers/TablistProvider.java
+++ src/main/java/es/hulk/survival/providers/TablistProvider.java
@@ -9,7 +9,6 @@ import es.hulk.tablist.TablistLayout;
 import es.hulk.tablist.utils.Skin;
 import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
-import org.bukkit.block.data.type.Bed;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
@@ -24,43 +23,44 @@ public class TablistProvider implements TablistAdapter {
         PlayerUtils playerUtils = new PlayerUtils(player);
         BedLocation bedLocation = new BedLocation(player);
 
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 2, "&aTus Estadisticas", Skin.getSkin(player)));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 3, "&bMobs Matados&7: &e" + playerUtils.getMobKills()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 4, "&bJugadores&7: &e" + playerUtils.getKills()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 5, "&bMuertes&7: &e" + playerUtils.getDeaths()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 6, "&bTiempo&7: &e" + playerUtils.getPlayTime()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 7, "&bVida&7: &e" + playerUtils.getHealth()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 2, "&6Tus Estadisticas", Skin.getSkin(player)));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 3, "&eMobs Matados&7: &a" + playerUtils.getMobKills()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 4, "&eJugadores&7: &a" + playerUtils.getKills()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 5, "&eMuertes&7: &a" + playerUtils.getDeaths()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 6, "&eTiempo&7: &a" + playerUtils.getPlayTime()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 7, "&eVida&7: &a" + playerUtils.getHealth()));
 
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 10, "&aBosses Eliminados"));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 11, "&bWhiters&7: &e" + player.getStatistic(Statistic.KILL_ENTITY, EntityType.WITHER)));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 12, "&bEnder Dragons&7: &e" + player.getStatistic(Statistic.KILL_ENTITY, EntityType.ENDER_DRAGON)));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 10, "&6Bosses Eliminados"));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 11, "&eWhiters&7: &a" + player.getStatistic(Statistic.KILL_ENTITY, EntityType.WITHER)));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 12, "&eEnder Dragons&7: &a" + player.getStatistic(Statistic.KILL_ENTITY, EntityType.ENDER_DRAGON)));
 
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 14, "&aTus Coordenadas"));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 15, "&bMundo&7: &e" + playerUtils.getWorld(), Skin.PLANET_SKIN));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 16, "&bBioma&7: &e" + playerUtils.getBiome()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 17, "&bX&7: &e" + player.getLocation().getBlockX()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 18, "&bY&7: &e" + player.getLocation().getBlockY()));
-        tabs.add(new TablistLayout(TablistColumn.LEFT, 19, "&bZ&7: &e" + player.getLocation().getBlockZ()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 14, "&6Tus Coordenadas"));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 15, "&eMundo&7: &a" + playerUtils.getWorld(), Skin.PLANET_SKIN));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 16, "&eBioma&7: &a" + playerUtils.getBiome()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 17, "&eX&7: &a" + player.getLocation().getBlockX()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 18, "&eY&7: &a" + player.getLocation().getBlockY()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 19, "&eZ&7: &a" + player.getLocation().getBlockZ()));
 
         if (player.getBedSpawnLocation() != null) {
-            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 3, "&aCoords de tu Cama"));
-            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 4, "&bMundo&7: &e" + bedLocation.getWorld(), Skin.PLANET_SKIN));
-            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 5, "&bX&7: &e" + bedLocation.getBlockX()));
-            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 6, "&bY&7: &e" + bedLocation.getBlockY()));
-            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 7, "&bZ&7: &e" + bedLocation.getBlockZ()));
+            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 3, "&6Coords de tu Cama"));
+            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 4, "&eMundo&7: &a" + bedLocation.getWorld(), Skin.PLANET_SKIN));
+            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 5, "&eX&7: &a" + bedLocation.getBlockX()));
+            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 6, "&eY&7: &a" + bedLocation.getBlockY()));
+            tabs.add(new TablistLayout(TablistColumn.MIDDLE, 7, "&eZ&7: &a" + bedLocation.getBlockZ()));
         }
 
-        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 11, "&bMundo&7: &eOverworld", Skin.PLANET_SKIN));
-        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 12, "&bX&7: &e" + player.getWorld().getSpawnLocation().getBlockX()));
-        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 13, "&bY&7: &e" + player.getWorld().getSpawnLocation().getBlockY()));
-        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 14, "&bZ&7: &e" + player.getWorld().getSpawnLocation().getBlockZ()));
+        tabs.add(new TablistLayout(TablistColumn.LEFT, 14, "&6Coords del Spawn"));
+        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 11, "&eMundo&7: &aOverworld", Skin.PLANET_SKIN));
+        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 12, "&eX&7: &a" + player.getWorld().getSpawnLocation().getBlockX()));
+        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 13, "&eY&7: &a" + player.getWorld().getSpawnLocation().getBlockY()));
+        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 14, "&eZ&7: &a" + player.getWorld().getSpawnLocation().getBlockZ()));
 
-        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 18, "&aNuestro Discord"));
-        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 19, "&bdiscord.frostpvp.net", Skin.DISCORD_SKIN));
+        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 18, "&6Nuestro Discord"));
+        tabs.add(new TablistLayout(TablistColumn.MIDDLE, 19, "&cNo Discord...", Skin.DISCORD_SKIN));
 
-        tabs.add(new TablistLayout(TablistColumn.RIGHT, 16, "&aInformacion del Servidor", Skin.COMPASS_SKIN));
-        tabs.add(new TablistLayout(TablistColumn.RIGHT, 17, "&bTu Ping&7: &e" + player.getPing() + "ms"));
-        tabs.add(new TablistLayout(TablistColumn.RIGHT, 18, "&bJugadores&7: &e" + Bukkit.getOnlinePlayers().size()));
+        tabs.add(new TablistLayout(TablistColumn.RIGHT, 16, "&6Informacion del Servidor", Skin.COMPASS_SKIN));
+        tabs.add(new TablistLayout(TablistColumn.RIGHT, 17, "&eTu Ping&7: &a" + player.getPing() + "ms"));
+        tabs.add(new TablistLayout(TablistColumn.RIGHT, 18, "&eJugadores&7: &a" + Bukkit.getOnlinePlayers().size()));
 
         return tabs;
     }
@@ -69,7 +69,7 @@ public class TablistProvider implements TablistAdapter {
     public List<String> getHeader(Player player) {
         return Arrays.asList(
                 "",
-                "&eBienvendo &a" + player.getName() + " &eal Survival",
+                "&6Bienvendo &a" + player.getName() + " &eal Survival",
                 "");
     }
 
@@ -77,7 +77,7 @@ public class TablistProvider implements TablistAdapter {
     public List<String> getFooter(Player player) {
         return Arrays.asList(
                 "",
-                "&eDisfruta de tu estancia en el servidor",
+                "&6Disfruta de tu estancia en el servidor",
                 "");
     }
 }
